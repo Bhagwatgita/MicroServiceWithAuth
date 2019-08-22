ebfacfb74ee1b69d09cf505ce1d8a138f1d3c9ac
Rabindra <rabindradahal2074@gmail.com>
2019-08-22 17:33:09 +0545

    Microservice created with auth

---
+++ AuthService/Controllers/AuthController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private List<User> _users = new List<User>
+        {
+            new User { AccountNumber = 1234567, Currency = "AUS", FullName = "Rajesh Khanna", Username = "rajesh", Password = "rajesh@123" },
+            new User { AccountNumber = 1239567, Currency = "NPR", FullName = "Ramesh Sharma", Username = "ramesh", Password = "ramesh@123" },
+            new User { AccountNumber = 1238567, Currency = "USD", FullName = "Durga Fuyal", Username = "durga", Password = "durga@123" },
+        };
+
+        [AllowAnonymous]
+        [HttpPost("authentication")]
+        public IActionResult Authentication([FromBody] Login loginParam)
+        {
+            var token = Authenticate(loginParam.Username, loginParam.Password);
+
+            if (token == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(token);
+        }
+
+        public SecurityToken Authenticate(string username, string password)
+        {
+            var user = _users.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            // return null if user not found
+            if (user == null)
+                return null;
+
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("3ce1637ed40041cd94d4853d3e766c4d");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("accountnumber", user.AccountNumber.ToString()),
+                    new Claim("currency", user.Currency),
+                    new Claim("name", user.FullName)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtSecurityToken = tokenHandler.WriteToken(token);
+
+            return new SecurityToken() { auth_token = jwtSecurityToken };
+        }
+
+    }
+    public class SecurityToken
+    {
+        public string auth_token { get; set; }
+    }
+    public class User
+    {
+        public int AccountNumber { get; set; }
+        public string FullName { get; set; }
+        public string Currency { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    public class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Gateway/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                          .UseStartup<Startup>()
+                          .ConfigureAppConfiguration((hostingContext, config) =>
+                          {
+                              config.AddJsonFile("configuration.json")
+                              .AddEnvironmentVariables();
+                          })
+                          .ConfigureServices(s =>
+                          {
+                              s.AddAuthentication(x =>
+                              {
+                                  x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                                  x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                              })
+                            .AddJwtBearer(x =>
+                            {
+                                x.RequireHttpsMetadata = false;
+                                x.SaveToken = true;
+                                x.TokenValidationParameters = new TokenValidationParameters
+                                {
+                                    ValidateIssuerSigningKey = true,
+                                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("3ce1637ed40041cd94d4853d3e766c4d")),
+                                    ValidateIssuer = false,
+                                    ValidateAudience = false
+                                };
+                            });
+                              s.AddOcelot();
+                          })
+                          .Configure(app =>
+                          {
+                              app.UseAuthentication();
+                              app.UseOcelot().Wait();
+                          })
+                          .ConfigureLogging((hostingContext, logging) =>
+                          {
+                              //add your logging
+                          })
+                          .UseIISIntegration()
+                          .Build();
+        }
+    }
+}
